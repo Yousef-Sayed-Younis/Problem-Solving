b3b414d8fff8c5cb0def4f958de1ef105bdd3aba
Problem Solver <98708975+Yousef-Sayed-Younis@users.noreply.github.com>
2023-02-26 04:15:02 +0200

    Create Distinct_Split.cpp

---
+++ Distinct_Split.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define start() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+int main()
+{
+    start();
+
+    int t; cin >> t;
+
+    while (t--) {
+        set<char> a, b;
+        int n; cin >> n;
+        string s; cin >> s;
+        int ar[26] = { 0 };
+
+        for (int i = 0; i < n; i++) ar[s[i]-97] += 1, b.insert(s[i]);
+
+        ll sum = 0;
+        for (int i = 0; i < n; i++) {
+            a.insert(s[i]);
+
+            if (ar[s[i] - 97] > 1) ar[s[i] - 97] -= 1; else b.erase(s[i]);
+
+            sum = max(sum, (ll) (a.size() + b.size()));
+        }
+
+        cout << sum << endl;
+    }
+
+    return 0;
+}
